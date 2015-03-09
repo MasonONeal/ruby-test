db89faafb0a39ccb333cdd5c16f822d31d99b859
Mason O'Neal <masonwid@gmail.com>
2015-03-09 14:33:50 -0700

    Add greeting and string input

--- talk.rb
+++ talk.rb
@@ -1,4 +1,9 @@
 # simple program that takes a comment or question and replies based on words used
 # user talks to the program
 
+system 'cls'
+puts
+puts "Welcome, friend"
+puts
+inut_string = gets.chomp
 
