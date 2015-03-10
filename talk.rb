1d83489b1d950614124c5524851b8d240adf4578
Mason O'Neal <masonwid@gmail.com>
2015-03-09 17:10:38 -0700

    Add reply based in input

--- talk.rb
+++ talk.rb
@@ -1,17 +1,20 @@
 # simple program that takes a comment or question and replies based on words used
 # user talks to the program
 
-# create array of responses
+# create array of responses. a hash would not work due to multiple possible responses for same input.
 
+# initialize variables
 responses = []
+
+
+
 File.open('name_responses.txt') do |f|
   f.each_line do |line|
-    pair_array = line.split(", ")
+    pair_array = line.split("} ")
     pair_array.map! {|string| string.tr("\n","")}
     responses << pair_array
   end
 end
-
 
 # show array
 puts "press enter when ready"
@@ -29,8 +32,19 @@ puts " Welcome, friend!"
 puts
 puts
 print " Please tell me your name: "
-inut_string = gets.chomp
+input_string = gets.chomp
 puts
+
+# program responds
+reply = []
+responses.each do |pair|
+  if input_string == pair[0]
+    reply << pair[1]
+  end
+end
+puts " " + reply.sample
+puts
+
 puts " How are you?"
 print " "
 inut_string = gets.chomp
